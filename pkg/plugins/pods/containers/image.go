8357744f4e583facf27dd769c5e46ff7a1c15b5c
XU <qqbuby@gmail.com>
2021-12-28 13:34:52 +0800

    modified:   containers/image.go

--- pkg/plugins/pods/containers/image.go
+++ pkg/plugins/pods/containers/image.go
@@ -20,7 +20,7 @@ type ImageRef struct {
 
 func ParseImageRef(image string) (*ImageRef, error) {
 	if !re.MatchString(image) {
-		return nil, fmt.Errorf("`image` must be (registry)/(org)/(name):(tag)[@digest]")
+		return nil, fmt.Errorf("`image` must match (registry)/(org)/(name):(tag)[@digest]")
 	}
 
 	matches := re.FindStringSubmatch(image)
